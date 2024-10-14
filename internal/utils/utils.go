4f24e430b5ebffa76b3b48a0ef62503304587b18
Igor Eliseev <igor.eliseev@simbirsoft.com>
2024-10-14 11:31:48 +0400

    Реализоввал подпись запроса и проверку подписи

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,5 +1,10 @@
 package utils
 
+import (
+	"flag"
+	"os"
+)
+
 func UInt64ToFloat64Ptr(i uint64) *float64 {
 	f := float64(i)
 	return &f
@@ -11,4 +16,13 @@ func AddFloat64Ptr(a, b *int64) *int64 {
 	}
 	sum := *a + *b
 	return &sum
+}
+
+func GetSecretKey(key string) *string {
+	secret := flag.String(key, "", "secret key")
+	flag.Parse()
+	if *secret == "" {
+		*secret = os.Getenv(key)
+	}
+	return secret
 }
